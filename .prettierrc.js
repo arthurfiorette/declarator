f13fb39b5d98dd6540e0c493aecf3e62aa041976
Hazork <arthur.fiorette@gmail.com>
2021-10-01 18:53:28 -0300

    init: project boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// https://github.com/ArthurFiorette/prettier-config
+
+module.exports = require('@arthurfiorette/prettier-config')({
+  tsdoc: true,
+  jsdocSpaces: 1,
+  jsdocPrintWidth: 70,
+  jsdocSingleLineComment: false
+});
